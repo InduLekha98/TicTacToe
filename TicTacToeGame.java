d0f4a7cdf25ed047d7fa85f9ee59b766030d6802
InduLekha98 <rindulekha09@gmail.com>
2021-02-20 04:23:46 +0530

    Added:Create a TicTacBoard

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,24 @@
 public class TicTacToeGame {
-		 public static void main( String[] args ) {
-			 	System.out.println("Welcome to TicTacToe problem");
-         }
+	public static char p_00;
+	public static char p_01;
+	public static char p_02;
+	public static char p_10;
+	public static char p_11;
+	public static char p_12;
+	public static char p_20;
+	public static char p_21;
+	public static char p_22;
+
+	public static void uc1_Initialize() {
+		char[] empty_Board = new char[10];
+		for (int i = 1; i <= 10; i++) {
+			empty_Board[i] = ' ';
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TicTacToe problem");
+		uc1_Initialize();
+	}
+
 }
